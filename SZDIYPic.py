7309133a598e24c75387bf083c3c81458cfa69b4
Jianer Shi (Paul) <hipaulshi@gmail.com>
2014-05-02 16:23:09 +0800

    Watermark is now Truetype with customizable font and location.

--- SZDIYPic.py
+++ SZDIYPic.py
@@ -16,7 +16,7 @@
 # along with szdiyCam.  If not, see <http://www.gnu.org/licenses/>.
 
 from config import TMPDIRECTORY, imagePath
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from PictureCamera import PictureCamera
 import time, os, sys
 
@@ -40,23 +40,27 @@ class SZDIYPic:
 
 		os.system('cp '+fileDirectory+'/'+fileName+' '+imagePath+'/'+date+ '/' + time + '.jpg')
 
-	def resizeImageAndApplyWaterMark (self,inputFileName, outputFileName, quality, enWaterMark=True):
+	def resizeImageAndApplyWaterMark (self,inputFileName, outputFileName, quality=85, enWaterMark=True, **waterMarkArgs):
 		print "working on img"
 		im = Image.open(TMPDIRECTORY+'/'+inputFileName)
 		
 		if enWaterMark:
-			draw = ImageDraw.Draw(im)
-			textPadding = 5
-			topLeftWidth = int(im.size[0] - (im.size[0] / 4))
-			topLeftHeight = int(im.size[1] - (im.size[1] / 10))
 			timeInfo = self.__getPicTimeStampString(inputFileName, TMPDIRECTORY, '%Y-%m-%d %H:%M:%S')
 
+			#pass in or define default font parameters
+			fontSize = waterMarkArgs['fontSize'] if waterMarkArgs['fontSize'] else 20
+			hLocation = waterMarkArgs['hLocation'] if waterMarkArgs['hLocation'] else 10
+			vLocation = waterMarkArgs['vLocation'] if waterMarkArgs['vLocation'] else 10
+
+			# use a truetype font
+			fontPath = os.path.dirname(os.path.abspath(__file__))+'/'+'KellySlab-Regular.ttf'
+			font = ImageFont.truetype(fontPath, fontSize)
+			
 			print "processing img..."
-			draw.text([topLeftWidth + textPadding, topLeftHeight + textPadding], timeInfo, fill="green")
-		
-		im.save(TMPDIRECTORY+'/'+outputFileName, 'JPEG', quality=quality)
-		
-		if enWaterMark:
+			draw = ImageDraw.Draw(im)
+			draw.text((hLocation, vLocation), timeInfo, font=font)
 			del draw
+
+		im.save(TMPDIRECTORY+'/'+outputFileName, 'JPEG', quality=quality)
 
 		del im
